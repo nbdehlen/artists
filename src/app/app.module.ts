392c2d8c3a7bebfac4daf953bed9d1f5ecc9d2ca
Bastian <nbdehlen@gmail.com>
2021-03-10 07:16:34 +0100

    Move list to components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FilterPipe } from './pipes/filter.pipe';
-import { ListComponent } from './shared/list/list.component';
+import { ListComponent } from './components/list/list.component';
 import { FormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
