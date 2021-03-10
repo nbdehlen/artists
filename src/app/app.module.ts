66c2a5ff85ba50836f5ac15b81744be311536eb2
Bastian <nbdehlen@gmail.com>
2021-03-10 06:08:58 +0100

    Mobile styli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { FilterPipe } from './pipes/filter.pipe';
 import { ListComponent } from './shared/list/list.component';
 import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [AppComponent, ListComponent, FilterPipe],
@@ -17,6 +18,7 @@ import { FormsModule } from '@angular/forms';
     GraphQLModule,
     HttpClientModule,
     FormsModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
